c6d087a04a7fa274757dbc979a193fce2e1fb05f
HD-139139 <gmlakd5944@gmail.com>
2021-08-09 14:59:08 +0900

    semi

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "my_instance" {
+  ami                    = "ami-0f49ee52a88cc2435"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.my_sg_web.id]
+  key_name               = aws_key_pair.my_sshkey.key_name
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./my_sshkey")
+    host        = self.public_ip
+  }
+
+  provisioner "file" {
+    source      = "site.yaml"
+    destination = "/home/ubuntu/site.yaml"
+  }
+
+  provisioner "file" {
+    source      = "roles"
+    destination = "/home/ubuntu/"
+  }
+
+  provisioner "file" {
+    source      = "group_vars"
+    destination = "/home/ubuntu/"
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOF
+      ssh-keyscan -t ssh-rsa ${self.public_ip} >> ~/.ssh/known_hosts
+      echo "${self.public_ip} ansbile_ssh_user=ubuntu ansible_ssh_private_key_file=./my_sshkey" > inventory.ini
+      echo "private_ip: ${self.public_ip}" >> group_vars/all.yaml
+      echo "service_port: ${var.service_port}" >> group_vars/all.yaml
+      sudo apt-get update
+      EOF
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory.ini site.yaml -b"
+  }
+}
+
+resource "aws_key_pair" "my_sshkey" {
+  key_name   = "my_sshkey"
+  public_key = file("./my_sshkey.pub")
+}
---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "ami_image" {
+  description = "ubuntu 20.04"
+  type        = string
+  default     = "ami-0f49ee52a88cc2435"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "service_port" {
+  description = "Wordpress Port"
+  type        = number
+  default     = 8080
+}
